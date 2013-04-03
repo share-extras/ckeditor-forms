c3f684d12f4eebe4a4cfc8fa4ae899c5f48c0413
Francois Liot <francois@liot.org>
2013-04-03 10:40:29 +0200

    Image width/height attributes rewritter

--- source/web/components/editors/ckeditor/config.js
+++ source/web/components/editors/ckeditor/config.js
@@ -13,4 +13,57 @@ CKEDITOR.editorConfig = function( config )
 		        ['Link','Unlink','Anchor'],
 		        ['Maximize','-','About']
 		        ];
+
+
+ CKEDITOR.on( 'instanceReady', function( ev )
+  {
+    var editor = ev.editor,
+    dataProcessor = editor.dataProcessor,
+    htmlFilter = dataProcessor && dataProcessor.htmlFilter;
+
+    // Output properties as attributes, not styles.
+    htmlFilter.addRules(
+    {
+      elements :
+      {
+        $ : function( element )
+        {
+          // Output dimensions of images as width and height
+          if ( element.name == 'img' )
+          {
+            var style = element.attributes.style;
+
+            if ( style )
+            {
+              // Get the width from the style.
+              var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec( style ),
+                width = match && match[1];
+
+              // Get the height from the style.
+              match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec( style );
+              var height = match && match[1];
+
+              if ( width )
+              {
+                element.attributes.style = element.attributes.style.replace( /(?:^|\s)width\s*:\s*(\d+)px;?/i , '' );
+                element.attributes.width = width;
+              }
+
+              if ( height )
+              {
+                element.attributes.style = element.attributes.style.replace( /(?:^|\s)height\s*:\s*(\d+)px;?/i , '' );
+                element.attributes.height = height;
+              }
+            }
+          }
+
+          if ( !element.attributes.style )
+            delete element.attributes.style;
+
+          return element;
+        }
+      }
+    });
+  });
+
 };
