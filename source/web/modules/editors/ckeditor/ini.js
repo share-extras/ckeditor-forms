495926bdd2a6de1927b807a4b55ec83ecc78e30c
touchvignesh <touchvignesh@gmail.com>
2013-09-03 21:41:04 +0530

    Update ini.js
    This fix will make CK editor to work in both Share repository and in site for both Alfresco 4.2.c and 4.2.d. Earlier it failed to work for files under Repository

--- source/web/modules/editors/ckeditor/ini.js
+++ source/web/modules/editors/ckeditor/ini.js
@@ -1,2 +1,3 @@
-// Fix for Alfresco Version 4.2.c and 4.2.d. This will make CK Editor to work in both Share Repository and in Share sites.
+// Fix for Alfresco Version 4.2.c and 4.2.d. 
+// This will make CK Editor to work in both Share Repository and in Share sites.
 var CKEDITOR_BASEPATH = '/share/res/modules/editors/ckeditor/';
